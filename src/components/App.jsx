23c7bdc67cf65f198e951a77acfb2ea3bd2e6345
kolya8077 <kolya8077@gmail.com>
2022-06-28 18:39:05 +0300

    task 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,41 @@
 import user from 'components/user';
 
-export const App = () => {
+function Profile(props) {
   return (
-    <div class="profile">
-      <div class="description">
-        <img
-          src="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
-          alt="User avatar"
-          class="avatar"
-        />
-        <p class="name">Petra Marica</p>
-        <p class="tag">@pmarica</p>
-        <p class="location">Salvador, Brasil</p>
+    <div className="prof__profile">
+      <div className="prof__description">
+        <img src={props.avatar} alt={props.username} className="prof__avatar" />
+        <p className="prof__name">{props.username}</p>
+        <p className="prof__tag">@{props.tag}</p>
+        <p className="prof__location">{props.location}</p>
       </div>
 
-      <ul class="stats">
-        <li>
-          <span class="label">Followers</span>
-          <span class="quantity">1000</span>
+      <ul className="prof__stats">
+        <li className="prof__list">
+          <span className="prof__label">Followers</span>
+          <span className="prof__quantity">{props.stats.followers}</span>
         </li>
-        <li>
-          <span class="label">Views</span>
-          <span class="quantity">2000</span>
+        <li className="prof__list">
+          <span className="prof__label">Views</span>
+          <span className="prof__quantity">{props.stats.views}</span>
         </li>
-        <li>
-          <span class="label">Likes</span>
-          <span class="quantity">3000</span>
+        <li className="prof__list">
+          <span className="prof__label">Likes</span>
+          <span className="prof__quantity">{props.stats.likes}</span>
         </li>
       </ul>
     </div>
+  );
+}
+
+export const App = () => {
+  return (
+    <Profile
+      username={user.username}
+      tag={user.tag}
+      location={user.location}
+      avatar={user.avatar}
+      stats={user.stats}
+    />
   );
 };
