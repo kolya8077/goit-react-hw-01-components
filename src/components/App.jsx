b792bb77627eca47f589dc93833b7cd350c72c0f
kolya8077 <kolya8077@gmail.com>
2022-06-28 14:17:16 +0300

    Creat user

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import user from 'components/user';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div class="profile">
+      <div class="description">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2922/2922506.png"
+          alt="User avatar"
+          class="avatar"
+        />
+        <p class="name">Petra Marica</p>
+        <p class="tag">@pmarica</p>
+        <p class="location">Salvador, Brasil</p>
+      </div>
+
+      <ul class="stats">
+        <li>
+          <span class="label">Followers</span>
+          <span class="quantity">1000</span>
+        </li>
+        <li>
+          <span class="label">Views</span>
+          <span class="quantity">2000</span>
+        </li>
+        <li>
+          <span class="label">Likes</span>
+          <span class="quantity">3000</span>
+        </li>
+      </ul>
     </div>
   );
 };
