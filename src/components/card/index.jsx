087b63ed12660466ebd5996184b8076ffc5d3584
kolya8077 <kolya8077@gmail.com>
2022-06-30 00:17:57 +0300

    css task 1

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -1,27 +1,31 @@
+import css from './style.module.css';
+
 export default function Profile({ avatar, username, tag, location, stats }) {
-  return (
-    <div className="prof__profile">
-      <div className="prof__description">
-        <img src={avatar} alt={username} className="prof__avatar" />
-        <p className="prof__name">{username}</p>
-        <p className="prof__tag">@{tag}</p>
-        <p className="prof__location">{location}</p>
+return (
+    <div className={css.profile}>
+      <div className={css.description}>
+      <img src={avatar} alt={username} className={css.avatar} />
+        <p className={css.name}>{username}</p>
+        <p className={css.tag}>@{tag}</p>
+        <p className={css.location}>{location}</p>
       </div>
 
-      <ul className="prof__stats">
-        <li className="prof__list">
-          <span className="prof__label">Followers</span>
-          <span className="prof__quantity">{stats.followers}</span>
+      <ul className={css.stats}>
+        <li className={css.list}>
+          <span className={css.label}>Followers</span>
+          <span className={css.quantity}>{stats.followers}</span>
         </li>
-        <li className="prof__list">
-          <span className="prof__label">Views</span>
-          <span className="prof__quantity">{stats.views}</span>
+        <li className={css.list}>
+          <span className={css.label}>Views</span>
+          <span className={css.quantity}>{stats.views}</span>
         </li>
-        <li className="prof__list">
-          <span className="prof__label">Likes</span>
-          <span className="prof__quantity">{stats.likes}</span>
+        <li className={css.list}>
+          <span className={css.label}>Likes</span>
+          <span className={css.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
+
+
